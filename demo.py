476a41726f24f39a63b08bb0ea4b3f7bddfce1c3
Kiran <Kiran.ds2022@gmail.com>
2025-02-02 22:23:58 +0530

    Configuration mongodb commection

---
+++ demo.py
@@ -0,0 +1,2 @@
+from us_visa_project.logger import logging  
+logging.info("logging has started")
--- template.py
+++ template.py
@@ -12,7 +12,7 @@ list_of_files=[
     f"{project_name}/components/model_trainer.py",
     f"{project_name}/components/model_evaluation.py",
     f"{project_name}/components/model_pusher.py",
-    f"{project_name}/config/__init__.py",
+    f"{project_name}/configuration/__init__.py",
     f"{project_name}/constants/__init__.py",
     f"{project_name}/entity/__init__.py",
     f"{project_name}/entity/config_entity.py",
